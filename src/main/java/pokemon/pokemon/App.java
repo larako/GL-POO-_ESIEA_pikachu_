d3ca1d4b77b379624535abb6b1f21e5f3a4ef34a
Soka <Soka@gmail.com>
2016-03-22 15:43:26 +0100

    Mis à jour projet actuel vers github:
    - Interface graphique
    - Début du moteur
    - Requêtes SQL

---
+++ src/main/java/pokemon/pokemon/App.java
@@ -0,0 +1,13 @@
+package pokemon.pokemon;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Pokemon poke = new Pokemon(1);
+    	//System.out.println(poke);
+		Window inter= new Window();
+    	
+    }
+}
